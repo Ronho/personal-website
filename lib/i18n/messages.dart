96de7f10c96a966a82b4c08ca43f6cad5229e040
Ronho <ronholzapfel@outlook.de>
2023-02-19 22:26:51 +0100

    fix: emoji

--- lib/i18n/messages.dart
+++ lib/i18n/messages.dart
@@ -10,7 +10,7 @@ class Messages extends Translations {
           'de_DE': 'Deutsch',
 
           // Different texts.
-          'welcome': 'Welcome 🤗, I am Ron!',
+          'welcome': 'Welcome \u0001F917, I am Ron!',
           'motivation_text': 'I have a passion for topics that are related to '
               'data and artificial intelligence. Reinforcement learning in '
               'particular has been a source of fascination for me. It is in '
